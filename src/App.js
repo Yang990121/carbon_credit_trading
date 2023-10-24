29f994e0645424eb69430fb0198f99edeeaf67e8
Reiko Lee <sinreiko@hotmail.com>
2023-10-24 17:39:24 +0800

    update src

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import CarbonCreditDashboard from './pages/CarbonCreditDashboard'
+import UserPage from './pages/UserPage.js'
+import { Web3Provider } from './contexts/Web3Context';
+// import EthereumAccount from './components/EthereumAccount';
+function App() {
+
+  return (
+    <Web3Provider>
+      <CarbonCreditDashboard />
+    </Web3Provider >
+  );
+}
+
+export default App;
